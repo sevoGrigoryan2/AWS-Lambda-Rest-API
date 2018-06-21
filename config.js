fbc048f9bee4d355d43cada38910809cc5369940
sevoGrigoryan <40152114+sevoGrigoryan@users.noreply.github.com>
2018-06-21 17:42:06 +0400

    CC

--- config.js
+++ config.js
@@ -5,3 +5,6 @@ module.exports = {
     queue:'https://sqs.us-east-1.amazonaws.com/578641730378/testSQS',
     phone: '+37493802210'
 };
+
+// User name,Password,Access key ID,Secret access key,Console login link
+// serverless-api,,AKIAIZBWGNETASFGX6FA,Nkcye9oGDPMzrz619+T5l8ctANWIQSzuHcBFIvFa,https://578641730378.signin.aws.amazon.com/console
